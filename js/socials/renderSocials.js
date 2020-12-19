becb25b23b21a793353e367022166e54ea39219b
Vaidmine <73877238+Vaidmine@users.noreply.github.com>
2020-12-19 19:32:32 +0200

    validations js

--- js/socials/renderSocials.js
+++ js/socials/renderSocials.js
@@ -1,16 +1,51 @@
 
 function renderSocials (data) {
+// input validation
+    if (Array.isArray(data) === false) {
+    //jei array uzrasymo tipas nera array, arba kitas galimas uzrasymas su invert(!) >>>  (!Array.isArray(data))
+    console.error('ERROR: social icons geenruoti reikia array tipo duomenu');
+    return false;
+
+    }
+
+
+// logic , kintamieji
+const socialsDOM = document.querySelector ('div > .socials-row');
+
+// ciklas
 let HTML = '';
 
     for (let i=0; i < data.length; i++) {
         const item = data [i];
+        if (typeof item !== 'object'){
+            continue; 
+// komanda "continue" - jei duomenu tipas netinkamas (ne objektas), tesiame toliau, t.y. pereiname prie sekancio 'saraso nario'
+        }
+        if (typeof item.link !== 'string' || item.link === '')
+        continue; 
+//komanda "continue" - jei duomenu tipas netinkamas (nera nuorodos string' arba nuoroda tuscia), tesiame toliau, t.y. pereiname prie sekancio 'saraso nario'
+        if (typeof item.icon !== 'string' || item.icon === '')
+        continue;   
+// komanda "continue" - jei duomenu tipas netinkamas (nera ikonos string'e arba ikona tuscia/ nera ikonos), tesiame toliau, t.y. pereiname prie sekancio 'saraso nario'
+
         HTML += `<a href="${item.link}" target="_blank" class="fa fa-${item.icon}"></a>`;
 
     }
-    const socialsDOM = document.querySelector ('div > .socials-row');
-    console.log(socialsDOM);
+ 
 
-    socialsDOM.innerHTML = HTML;
+
+
+// post logic validation, tikrinama ar sarasas tinkamas, ar nera tuscias ir pan.
+if (HTML === '') {
+    console.error('ERROR: napvyko sugeneruoti social ikonu / nuorodu');
+    return false;
+} 
+
+
+
+// return / turinio istatymas 
+socialsDOM.innerHTML = HTML;
+return true;
 }
 
 
